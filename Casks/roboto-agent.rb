39acdf3101d68a9829cd339117631ebbe4e46f13
John Hubberts <hubberts@roboto.ai>
2024-07-17 16:58:45 -0700

    v0.5.4

---
+++ Casks/roboto-agent.rb
@@ -0,0 +1,48 @@
+cask "roboto-agent" do
+  # Force commit
+  module Utils
+    @@os = OS.mac? ? "macos" : OS.kernel_name.downcase
+    @@arch = Hardware::CPU.arch.to_s.sub("arm64", "aarch64")
+    def self.os
+      @@os
+    end
+    def self.arch
+      @@arch
+    end
+    def self.binary
+      "roboto-agent-#{@@os}-#{@@arch}"
+    end
+  end
+
+  version "0.5.4"
+  if OS.mac?
+    sha256 arm: "c8358ea7dd7bc2657cfd2c8b23625275a7706fe97c1687201430a04c5df2711f",
+           intel: "9f1cab9ebdeb0a18d5a85a81f97015b74e54fcea621faf4d472b40d02e77bac3"
+  else
+    # Casks not supported on Linux: https://github.com/Linuxbrew/brew/issues/742
+    # sha256 arm: "...",
+    #        intel: "..."
+  end
+  url "https://github.com/roboto-ai/roboto-python-sdk/releases/download/v#{version}/roboto-agent-#{Utils.os}-#{Utils.arch}",
+      verified: "https://github.com/roboto-ai/"
+
+  name "Roboto Agent"
+  desc "Device agent for automatically uploading data to Roboto"
+  homepage "https://roboto.ai"
+
+  depends_on arch: [:arm64, :x86_64]
+
+  binary Utils.binary, target: "roboto-agent"
+
+  preflight do
+    target = config.binarydir / "roboto-agent"
+    if target.exist? && !target.symlink?
+      opoo "replacing self-updated #{target}"
+      target.delete
+    end
+  end
+
+  postflight do
+    Quarantine.release!(download_path: "#{caskroom_path}/#{version}/#{Utils.binary}") if Quarantine.available?
+  end
+end
---
+++ Casks/roboto.rb
@@ -0,0 +1,47 @@
+cask "roboto" do
+  module Utils
+    @@os = OS.mac? ? "macos" : OS.kernel_name.downcase
+    @@arch = Hardware::CPU.arch.to_s.sub("arm64", "aarch64")
+    def self.os
+      @@os
+    end
+    def self.arch
+      @@arch
+    end
+    def self.binary
+      "roboto-#{@@os}-#{@@arch}"
+    end
+  end
+
+  version "0.5.4"
+  if OS.mac?
+    sha256 arm: "e71d658e849dc740bc97a8a48b30f14c7dbe460bf3bb734a0b9e3139b5560882",
+           intel: "5217cbe6badf81a3cbd06cd769fd1e9de50c1eb9b22e49496c48337dc1fb5849"
+  else
+    # Casks not supported on Linux: https://github.com/Linuxbrew/brew/issues/742
+    # sha256 arm: "...",
+    #        intel: "..."
+  end
+  url "https://github.com/roboto-ai/roboto-python-sdk/releases/download/v#{version}/roboto-#{Utils.os}-#{Utils.arch}",
+      verified: "https://github.com/roboto-ai/"
+
+  name "Roboto"
+  desc "Command line interface for interacting with Roboto AI"
+  homepage "https://roboto.ai"
+
+  depends_on arch: [:arm64, :x86_64]
+
+  binary Utils.binary, target: "roboto"
+
+  preflight do
+    target = config.binarydir / "roboto"
+    if target.exist? && !target.symlink?
+      opoo "replacing self-updated #{target}"
+      target.delete
+    end
+  end
+
+  postflight do
+    Quarantine.release!(download_path: "#{caskroom_path}/#{version}/#{Utils.binary}") if Quarantine.available?
+  end
+end
