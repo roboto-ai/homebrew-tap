e324da318507db596692df7d4087b2f02ad4bd20
Josh Hawthorne <josh@roboto.dev>
2026-04-20 18:33:00 -0700

    Bump roboto to v0.40.0

--- Casks/roboto-agent.rb
+++ Casks/roboto-agent.rb
@@ -14,7 +14,7 @@ cask "roboto-agent" do
     end
   end
 
-  version "0.39.0"
+  version "0.40.0"
   if OS.mac?
     sha256 arm: "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5",
            intel: "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
