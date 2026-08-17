c78818d47b20a0e80f1b2136c102a6b6c8b4f1c3
Ruslan Dimov <ruslan@roboto.ai>
2026-08-17 14:01:23 -0700

    v0.54.0

--- Casks/roboto-agent.rb
+++ Casks/roboto-agent.rb
@@ -14,10 +14,10 @@ cask "roboto-agent" do
     end
   end
 
-  version "0.51.0"
+  version "0.54.0"
   if OS.mac?
-    sha256 arm: "0060c4f4a16ec0f126db6a936a766d8c65bea194fdf21cf33fb8c3edb7cff151",
-           intel: "1b39acd10256a49cff2022bad07dd8aa55ca9c3ec05ed94233a86afd52a136cd"
+    sha256 arm: "11102b9f164ac8dbd10c0d84e96ddb51a9a4b3a89cd1218bb2d15aac0313e2ad",
+           intel: "6e39fff360bc73ed9cfe973b440f3268e9feaae618ff46d6128abf346fac0110"
   else
     # Casks not supported on Linux: https://github.com/Linuxbrew/brew/issues/742
     # sha256 arm: "...",
--- Casks/roboto.rb
+++ Casks/roboto.rb
@@ -13,10 +13,10 @@ cask "roboto" do
     end
   end
 
-  version "0.51.0"
+  version "0.54.0"
   if OS.mac?
-    sha256 arm: "cea5425b2314d6972fc4ff93e56f6ec62e4c255f05325aacddf0fd3157e2e185",
-           intel: "2e2ca86497c3f6844b77657a2beabc95418a3ca2dfd7bd8abeff6047bba613ae"
+    sha256 arm: "b6b639eadecd0336384bebcf62600d2832c8cdefe2bf61f021de1c97f12fd0a5",
+           intel: "a87898f64fc1dfd9bba7720fff1123c2f4afb34e1f339ed1b4b7b8ee6b3f9719"
   else
     # Casks not supported on Linux: https://github.com/Linuxbrew/brew/issues/742
     # sha256 arm: "...",
